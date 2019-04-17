587d897185174ea3135a8a09ee9dc6f4ca6db980
Patric Khiev <webdev.patric@gmail.com>
2019-04-17 16:07:50 -0700

    sass added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import routes from './routes';
-import './App.css';
+import './App.scss';
 import store from './ducks/store';
 
 class App extends Component {
--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import './Landing.scss';
 
 class Landing extends Component {
+  constructor(){
+    super()
+      this.state = {
+        homes: []
+      }
+  }
+
+  componentDidMount(){
+    //axios.get(`/api/homes`).then(results => this.setState({homes: results.data})
+  }
   render() {
+    console.log(this.state.homes)
     return (
       <div>
-        <h1>Landing</h1>
-        <Link to='/admin'> <button >Go To Admin Page</button></Link>
+        <h1>Client's Landing</h1>
+        <Link to='/admin'><button>Go To Admin Page</button></Link>
+        <div className='listings'>
+          <h3>Listings</h3>
+        </div>
       </div>
     );
   }
