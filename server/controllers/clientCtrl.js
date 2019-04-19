11c469914fd118b3804037944e08f300516c5ed8
Daniel De Hoyos <dsdehoyos@gmail.com>
2019-04-19 13:35:25 -0500

    added post and post sass file

--- server/controllers/clientCtrl.js
+++ server/controllers/clientCtrl.js
@@ -1,10 +1,10 @@
 module.exports = {
-  getAllProperties: (req, res, next) =>{
-    const db = req.app.get('db');
+  getAllProperties: (req, res, next) => {
+    const db = req.app.get("db");
     db.get_all_properties()
       .then(response => {
         res.status(200).json(response);
       })
       .catch(err => res.status(500).send(err));
-  },
-}
+  }
+};
