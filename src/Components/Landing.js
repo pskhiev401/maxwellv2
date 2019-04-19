08720c9977a48216ad9abc20eacc6be86b5079d5
Daniel De Hoyos <dsdehoyos@gmail.com>
2019-04-19 16:20:55 -0500

    added post component

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,31 +1,33 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import './Landing.scss';
-import axios from 'axios';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./Landing.scss";
+import axios from "axios";
 
 class Landing extends Component {
-  constructor(){
-    super()
-      this.state = {
-        homes: []
-      }
+  constructor() {
+    super();
+    this.state = {
+      homes: []
+    };
   }
 
-  componentDidMount(){
-    axios.get('/api/getAll').then(res => {
-      this.setState({homes: res.data})
-    })
+  componentDidMount() {
+    axios.get("/api/getAll").then(res => {
+      this.setState({ homes: res.data });
+    });
   }
 
   render() {
-    console.log(this.state.homes)
+    console.log(this.state.homes);
     return (
       <div>
-        <h1 className='landing_cover'>Clients Custom Homes</h1>
-        <div className='listings'>
+        <h1 className="landing_cover">Clients Custom Homes</h1>
+        <div className="listings">
           <h3>Listings</h3>
         </div>
-        <Link to='/admin'><button>Go To Admin Page</button></Link>
+        <Link to="/admin">
+          <button>Go To Admin Page</button>
+        </Link>
       </div>
     );
   }
