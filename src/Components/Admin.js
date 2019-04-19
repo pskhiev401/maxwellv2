ea9face2ad8d4244b12746fd210fcaa40d1e50a3
Patric Khiev <webdev.patric@gmail.com>
2019-04-19 11:31:55 -0700

    just added sass file

--- src/Components/Admin.js
+++ src/Components/Admin.js
@@ -4,8 +4,10 @@ class Admin extends Component {
   render() {
     return (
       <div>
-        <h1>Admin Page</h1>
-        <button onClick={() => {}}>Click</button>
+        <h1>Admin Dashboard</h1>
+        <div className='post_button'>Post New Listing</div>
+        <div className='edit_button'>Edit Listing</div>
+        
       </div>
     );
   }
