01c364ce9e285cec48e6f7699b67ac0ab85bd4c3
Patric Khiev <webdev.patric@gmail.com>
2019-05-04 12:15:45 -0700

    contact form in progress

--- src/Components/landing/Landing.js
+++ src/Components/landing/Landing.js
@@ -7,7 +7,11 @@ class Landing extends Component {
   constructor() {
     super();
     this.state = {
-      homes: []
+      homes: [],
+      name: '',
+      email: '',
+      phone: '',
+      message: ''
     };
   }
 
@@ -33,12 +37,52 @@ class Landing extends Component {
       <div>
         <div className='landing_top'> Logo Section </div>
         <div className='navbar_section'>{navbar_items}</div>
-        <div className='landing_cover'>
-          <h3>Cover Image</h3>
-        </div>
         <Link to='/admin'>
           <button>Go To Admin Page</button>
         </Link>
+        <div className='landing_cover'>
+          <h3>Cover Image</h3>
+        </div>
+        {/* <div className='dfw__map'>Map</div>
+        <div className='about__us'>About Us</div> */}
+        <section className='contact_form'>
+          <h3>Contact Us</h3>
+          <input
+            required
+            type='text'
+            name='name'
+            id='name'
+            placeholder='Name'
+            value={this.state.name}
+            onChange={e => this.setState({ name: e.target.value })}
+          />
+          <input
+            required
+            type='email'
+            name='email'
+            id='email'
+            placeholder='Email'
+            value={this.state.email}
+            onChange={e => this.setState({ email: e.target.value })}
+          />
+          <input
+            type='text'
+            name='phone'
+            id='phone'
+            placeholder='Phone'
+            value={this.state.phone}
+            onChange={e => this.setState({ name: e.target.value })}
+          />
+          <textarea
+            required
+            name='message'
+            id='message'
+            rows='4'
+            placeholder='Message'
+            value={this.state.message}
+            onChange={e => this.setState({ message: e.target.value })}
+          />
+        </section>
       </div>
     );
   }
